03fe94b247b276dfa2019dc691b7cf3a77249dd2
cranajit <cranajit@amazon.com>
2019-10-23 21:36:28 +0530

    fixed bugs

--- app/app/addition.py
+++ app/app/addition.py
@@ -1,11 +1,12 @@
 def add_numbers(x, y):
     """ add two numbers and return the result"""
-    return x+y
+    return x + y
+
 
 def subtract_numbers(x, y):
     if x > y:
-        return x-y
-    elif x<y:
-        return y-x
+        return x - y
+    elif x < y:
+        return y - x
     else:
         return 0
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 from app.addition import add_numbers
 from app.addition import subtract_numbers
 
+
 class Addition_tests(TestCase):
     def test_add_numbers(self):
         """test the result of addition of two numbers"""
-        self.assertEqual(add_numbers(2, 3),5)
+        self.assertEqual(add_numbers(2, 3), 5)
 
     def test_substract_numbers(self):
         """test the result of substraction of two numbers"""
